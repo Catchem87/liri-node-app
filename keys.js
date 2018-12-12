23dc93195230963043538a8740c3d9d0f780a3bc
Catchem87 <moras.ashley87@gmail.com>
2018-12-12 14:35:23 -0500

    spotify-this-song

--- keys.js
+++ keys.js
@@ -5,6 +5,6 @@ exports.spotify = {
     secret: process.env.SPOTIFY_SECRET
 };
 
-//
+/*
 http://www.omdbapi.com/?apikey=[59cb00bb]&
-//
+*/
--- liri.js
+++ liri.js
@@ -1,1 +1,43 @@
 require("dotenv").config();
+
+// const result = dotenv.config();
+
+// if(result.error) {
+//     throw result.error
+// };
+
+// console.log(result.parsed);
+
+var keys = require("./keys.js");
+
+var Spotify = require('node-spotify-api');
+
+var command = process.argv[2];
+var input = process.argv.splice(3).join(' ');
+
+
+
+function spotifyThisSong() {
+    var spotify = new Spotify(keys.spotify);
+
+    spotify.search({
+        type: 'track',
+        query: input
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        // console.log(data.tracks.items);
+    
+        for(var i = 0; i < data.tracks.items.length; i++) {
+            
+            console.log('Data: ' + data.tracks.items[i].artists);
+            // console.log('Album name: ' + data.tracks.items[i].album.name);
+        }
+    });
+};
+
+
+if(command === "spotify-this-song") {
+    spotifyThisSong();
+}
