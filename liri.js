ae3581d7d128de13e9fa28cb1b59d33c5b68c127
Catchem87 <moras.ashley87@gmail.com>
2018-12-12 23:29:15 -0500

    do-what-it-says

--- liri.js
+++ liri.js
@@ -17,18 +17,21 @@ var command = process.argv[2];
 var input = process.argv.slice(3).join(' ');
 
 function movieThis() {
+
+    if (!input) {
+        input = "Mr. Nobody";
+    }
     axois.get("http://www.omdbapi.com/?t=" + input + "&apikey=59cb00bb").then(
-        function(response) {
-            console.log(response.data);
+        function (response) {
             var data = response.data;
             console.log("Title: " + data.Title + "\nYear: " + data.Year + "\nIMDB Rating: " + data.imdbRating + "\nCountry: " + data.Country + "\nLanguage: " + data.Language + "\nPlot: " + data.Plot + "\nActors: " + data.Actors);
         },
-        function(error) {
-            if(error.response) {
+        function (error) {
+            if (error.response) {
                 console.log(error.response.data);
                 console.log(error.response.status);
                 console.log(error.response.headers);
-            } else if(error.request) {
+            } else if (error.request) {
                 console.log(error.request);
             } else {
                 console.log("Error", error.message);
@@ -40,20 +43,20 @@ function movieThis() {
 
 function concertThis() {
     axois.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=de3b2d5fb097c9be2422bde5146a2f8f").then(
-        function(response) {
-            for(var y = 0; y < response.data.length; y++) {
+        function (response) {
+            for (var y = 0; y < response.data.length; y++) {
                 // console.log(response.data);
                 response.data.map(item => {
-                    console.log('\nVenue Name: ' + item.venue.name + '\nVenue Location: ' + item.venue.city + ', ' + item.venue.country + '\nDate: ' + moment(item.datetime).format("MM/DD/YYYY") );
+                    console.log('\nVenue Name: ' + item.venue.name + '\nVenue Location: ' + item.venue.city + ', ' + item.venue.country + '\nDate: ' + moment(item.datetime).format("MM/DD/YYYY"));
                 })
             }
         },
-        function(error) {
-            if(error.response) {
+        function (error) {
+            if (error.response) {
                 console.log(error.response.data);
                 console.log(error.response.status);
                 console.log(error.response.headers);
-            } else if(error.request) {
+            } else if (error.request) {
                 console.log(error.request);
             } else {
                 console.log("Error", error.message);
@@ -68,6 +71,10 @@ function concertThis() {
 function spotifyThisSong() {
     var spotify = new Spotify(keys.spotify);
 
+    if (!input) {
+        input = "The Sign Ace of Base"
+    }
+
     spotify.search({
         type: 'track',
         query: input
@@ -75,7 +82,7 @@ function spotifyThisSong() {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        
+
         for (var i = 0; i < data.tracks.items.length; i++) {
             //console.log(data.tracks.items[i].artists);
             data.tracks.items[i].artists.map(track => {
@@ -98,6 +105,24 @@ if (command === "movie-this") {
     movieThis();
 }
 
-if(command === "do-what-it-says") {
+if (command === "do-what-it-says") {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            console.log(error);
+        }
+        console.log(data);
 
+        var dataArr = data.split(",");
+
+        command = (dataArr[0]);
+        input = (dataArr[1]);
+
+        if (command === "spotify-this-song") {
+            spotifyThisSong();
+        } else if (command === "concert-this") {
+            concertThis();
+        } else if (command === "movie-this") {
+            movieThis();
+        }
+    })
 }
